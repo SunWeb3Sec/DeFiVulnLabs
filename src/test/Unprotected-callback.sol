dd190d9f8d57c5801445706050bc9d9e470b9a74
KirinFilip <72068235+KirinFilip@users.noreply.github.com>
2023-04-27 17:29:13 +0200

    Formatting and remove unnecessary deployment
    Remove unnecessary duplicate contract deployment of `MaxMint721Contract = new MaxMint721();`

--- src/test/Unprotected-callback.sol
+++ src/test/Unprotected-callback.sol
@@ -7,45 +7,38 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract ContractTest is Test {
     MaxMint721 MaxMint721Contract;
     bool complete;
-    uint maxMints = 10;
+    uint256 maxMints = 10;
     address alice = vm.addr(1);
     address eve = vm.addr(2);
 
     function testSafeMint() public {
-    MaxMint721Contract = new MaxMint721();
-    MaxMint721Contract = new MaxMint721();
-    MaxMint721Contract.mint(maxMints);
-    console.log("Bypassed maxMints, we got 19 NFTs");
-    console.log("NFT minted:", MaxMint721Contract.balanceOf(address(this)));
-
+        MaxMint721Contract = new MaxMint721();
+        MaxMint721Contract.mint(maxMints);
+        console.log("Bypassed maxMints, we got 19 NFTs");
+        console.log("NFT minted:", MaxMint721Contract.balanceOf(address(this)));
     }
-    function onERC721Received(
-    address,
-    address,
-    uint256,
-    bytes memory
-  ) public returns (bytes4) {
-    if (!complete) {
-      complete = true;
-      MaxMint721Contract.mint(maxMints - 1);
-    }
-    return this.onERC721Received.selector;
-  }
-    receive() payable external{}
 
+    function onERC721Received(address, address, uint256, bytes memory) public returns (bytes4) {
+        if (!complete) {
+            complete = true;
+            MaxMint721Contract.mint(maxMints - 1);
+        }
+        return this.onERC721Received.selector;
+    }
+
+    receive() external payable {}
 }
 
-
 contract MaxMint721 is ERC721Enumerable {
-  uint public MAX_PER_USER = 10;
+    uint256 public MAX_PER_USER = 10;
 
-  constructor() ERC721("ERC721","ERC721") {}
+    constructor() ERC721("ERC721", "ERC721") {}
 
-  function mint(uint amount) external {
-    require(balanceOf(msg.sender) + amount <= MAX_PER_USER, "exceed max per user");
-    for (uint256 i = 0; i < amount; i++) {
-      uint mintIndex = totalSupply();
-      _safeMint(msg.sender, mintIndex);
+    function mint(uint256 amount) external {
+        require(balanceOf(msg.sender) + amount <= MAX_PER_USER, "exceed max per user");
+        for (uint256 i = 0; i < amount; i++) {
+            uint256 mintIndex = totalSupply();
+            _safeMint(msg.sender, mintIndex);
+        }
     }
-  }
 }
