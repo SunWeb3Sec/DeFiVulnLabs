4a5368078270d97c3f49a4fc1ed22c1d11b0c76f
zhouxianyuan <zxy965211@163.com>
2023-07-19 21:31:10 +0800

    update ERC777 Reentrancy

--- src/test/ERC777-reentrancy.sol
+++ src/test/ERC777-reentrancy.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 //Maximum mints is 1,000, How can you bypass this limitation?
 
 contract MyERC777 is ERC777 , Test{
-    uint maxMints = 1000;
+    uint maxMints = 10000;
     constructor(uint256 initialSupply) ERC777("Gold", "GLD", new address[](0)) {
             _mint(msg.sender, initialSupply, "", "");
     }
