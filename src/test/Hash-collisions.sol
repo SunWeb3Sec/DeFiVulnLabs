5cbefa7a6d9c553288b09619f9e596de5702c9e0
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 21:55:41 +0800

    Update Hash-collisions.sol

--- src/test/Hash-collisions.sol
+++ src/test/Hash-collisions.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.15;
 import "forge-std/Test.sol";
 
 /*
-Demo: abi.encodePacked() Hash Collisions
+Name: abi.encodePacked() Hash Collisions
 
+Description:
 Using abi.encodePacked() with multiple variable length arguments can, 
 in certain situations, lead to a hash collision.
 
@@ -14,17 +15,17 @@ but collisions can still occur due to limitations in the hash function's size or
 This is a known issue mentioned:
 https://docs.soliditylang.org/en/v0.8.17/abi-spec.html?highlight=collisions#non-standard-packed-mode
 
-
 In deposit function allows users to deposit Ether into the contract based on two string inputs: _string1 and _string2. 
 The contract uses the keccak256 function to generate a unique hash by concatenating these two strings.
 
 If two different combinations of _string1 and _string2 produce the same hash value, a hash collision will occur. 
 The code does not handle this scenario properly and allows the second depositor to overwrite the previous deposit.
 
-Mitigation  
+Mitigation: 
 use of abi.encode() instead of abi.encodePacked()
 
 REF:
+https://twitter.com/1nf0s3cpt/status/1676476475191750656
 https://docs.soliditylang.org/en/v0.8.17/abi-spec.html?highlight=collisions#non-standard-packed-mode
 https://swcregistry.io/docs/SWC-133
 https://github.com/sherlock-audit/2022-10-nftport-judging/issues/118
