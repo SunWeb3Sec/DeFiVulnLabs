148f7fab395943622c1523ee98640a0b08b0b3cd
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 21:58:49 +0800

    Update NFT-transfer.sol

--- src/test/NFT-transfer.sol
+++ src/test/NFT-transfer.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
-Demo: Unauthorized NFT Transfer in custom ERC721 implementation.
+Name: Unauthorized NFT Transfer in custom ERC721 implementation.
 
+Description:
 Custom transferFrom function in contract VulnerableERC721, 
 does not properly check if msg.sender is the current owner of the token or an approved address.
 As a result, any address can call the transferFrom function to transfer any token, 
@@ -15,14 +16,14 @@ regardless of who the current owner is.
 This allows unauthorized users to transfer tokens they do not own, leading to potential theft of assets.
 
  
-Mitigation  
+Mitigation:  
 To ensure that msg.sender is the current owner of the token or an approved address.
 
 REF:
+https://twitter.com/1nf0s3cpt/status/1679120390281412609
 https://blog.decurity.io/scanning-for-vulnerable-erc721-implementations-fe19200b91b5
 https://ventral.digital/posts/2022/8/18/sznsdaos-bountyboard-unauthorized-transferfrom-vulnerability
 https://github.com/pessimistic-io/slitherin/blob/master/docs/nft_approve_warning.md
-
 */
 
 contract ContractTest is Test {
