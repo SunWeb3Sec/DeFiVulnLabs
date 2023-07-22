69ac1ca63cfcc89607e83ffbbabaf87e8a2b1472
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 21:40:30 +0800

    Update Returnfalse.sol

--- src/test/Returnfalse.sol
+++ src/test/Returnfalse.sol
@@ -7,7 +7,9 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /*
+Name: No Revert on Failure
 
+Description:
 Some tokens do not revert on failure, but instead return false (e.g. ZRX).
 
 ZRX transfer return false:
