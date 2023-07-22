081d219af8db39227b47e0fda36776f9e8a17867
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 09:12:47 +0800

    Update Delegatecall.sol

--- src/test/Delegatecall.sol
+++ src/test/Delegatecall.sol
@@ -3,9 +3,28 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
 
-// Proxy Contract is designed for helping users call logic contract
-// Proxy Contract's owner is hardcoded as 0xdeadbeef
-// Can you manipulate Proxy Contract's owner ?
+/*
+Name: Unsafe Delegatecall Vulnerability
+
+Description:
+The Proxy Contract Owner Manipulation Vulnerability is a flaw in the smart contract design that
+allows an attacker to manipulate the owner of the Proxy contract, which is hardcoded as 0xdeadbeef.
+The vulnerability arises due to the use of delegatecall in the fallback function of the Proxy contract. 
+delegatecall allows an attacker to invoke the pwn() function from the Delegate contract within the context 
+of the Proxy contract, thereby changing the value of the owner state variable of the Proxy contract.
+
+Scenario:
+Proxy Contract is designed for helping users call logic contract
+Proxy Contract's owner is hardcoded as 0xdeadbeef
+Can you manipulate Proxy Contract's owner ?
+
+Mitigation:
+To mitigate the Proxy Contract Owner Manipulation Vulnerability, 
+avoid using delegatecall unless it is explicitly required, and ensure that the delegatecall is used securely. 
+If the delegatecall is necessary for the contract's functionality, make sure to validate and 
+sanitize inputs to avoid unexpected behaviors.
+*/
+ 
 
 contract Proxy {
     address public owner = address(0xdeadbeef); // slot0
