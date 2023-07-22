26114e0dbd7910cbc89641b171d8b86e7c153c21
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 20:57:39 +0800

    Update txorigin.sol

--- src/test/txorigin.sol
+++ src/test/txorigin.sol
@@ -4,17 +4,29 @@ pragma solidity ^0.8.15;
 import "forge-std/Test.sol";
 
 /*
+
+Name: Insecure tx.origin Vulnerability
+
+Description:
+tx.origin is a global variable in Solidity; using this variable for authentication in 
+a smart contract makes the contract vulnerable to phishing attacks.
+
+Scenario:
 Wallet is a simple contract where only the owner should be able to transfer
 Ether to another address. Wallet.transfer() uses tx.origin to check that the
 caller is the owner. Let's see how we can hack this contract
-*/
 
-/*
 What happened?
 Alice was tricked into calling Attack.attack(). Inside Attack.attack(), it
 requested a transfer of all funds in Alice's wallet to Eve's address.
 Since tx.origin in Wallet.transfer() is equal to Alice's address,
 it authorized the transfer. The wallet transferred all Ether to Eve.
+
+Mitigation:
+It is advisable to use msg.sender.
+
+REF:
+https://hackernoon.com/hacking-solidity-contracts-using-txorigin-for-authorization-are-vulnerable-to-phishing
 */
 
 contract ContractTest is Test {
