302b999d2b893b8feedaf270196a4aed1293cc06
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 21:57:33 +0800

    Update gas-price.sol

--- src/test/gas-price.sol
+++ src/test/gas-price.sol
@@ -4,20 +4,22 @@ pragma solidity ^0.8.18;
 import "forge-std/Test.sol";
 
 /*
-Demo: txGasPrice manipulation
+Name: txGasPrice manipulation
 
+Description:
 Manipulation of the txGasPrice value, which can result in unintended consequences and potential financial losses.
 
 In the calculateTotalFee function, the total fee is calculated by multiplying gasUsed + GAS_OVERHEAD_NATIVE with txGasPrice. 
 The issue is that the txGasPrice value can be manipulated by an attacker, potentially leading to an inflated fee calculation.
 
-Mitigation  
+Mitigation:  
 To address this vulnerability, it is recommended to implement safeguards such as using a gas oracle to obtain the average gas price from a trusted source. 
 
 Test:
 forge test --contracts src/test/gas-price.sol  -vvvv --gas-price 200000000000000
 
 REF:
+https://twitter.com/1nf0s3cpt/status/1678268482641870849
 https://github.com/solodit/solodit_content/blob/main/reports/ZachObront/2023-03-21-Alligator.md
 https://github.com/solodit/solodit_content/blob/main/reports/Trust%20Security/2023-05-15-Brahma.md
 https://blog.pessimistic.io/ethereum-alarm-clock-exploit-final-thoughts-21334987c331
