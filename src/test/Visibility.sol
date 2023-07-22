b4261a672f8bed6f81d56bf7e9fc25391a695c2a
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 20:54:55 +0800

    Update Visibility.sol

--- src/test/Visibility.sol
+++ src/test/Visibility.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-
-// this excersise is about wrong visibility of function resulting in access control issue
-// impact: the owner of the contract can be changed by anyone
 import "forge-std/Test.sol";
+
+/*
+Name: Improper Access Control Vulnerability
+
+Description:
+The default visibility of the function is Public. 
+If there is an unsafe visibility setting, the attacker can directly call the sensitive function in the smart contract.
+
+The ownerGame contract has a changeOwner function that is intended to change the owner of the contract.
+However, due to improper access control, this function is publicly accessible and 
+can be called by any external account or contract. As a result, an attacker can call this function
+to change the ownership of the contract and take control.
+
+Impact: the owner of the contract can be changed by anyone.
+
+Mitigation:
+Use access control modifiers: Solidity provides modifiers, such as onlyOwner, 
+which can be used to restrict the access of functions
+ 
+*/
 
 contract ContractTest is Test {
     ownerGame ownerGameContract;
