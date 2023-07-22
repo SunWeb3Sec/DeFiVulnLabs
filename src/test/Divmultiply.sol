53da830bfec216f92369b3334d05332dd57dcbbb
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 21:37:56 +0800

    Update Divmultiply.sol

--- src/test/Divmultiply.sol
+++ src/test/Divmultiply.sol
@@ -3,6 +3,29 @@ pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
 
+/*
+Name: Precision Issues - Divide before multiply
+
+Description:
+The contracts demonstrate a common issue when performing division operations in Solidity, 
+as Solidity doesn't support floating-point numbers. The order of operations can affect the result due to integer truncation.
+
+In the Miscalculation contract, the function price performs the division before the
+multiplication (price / 100) * discount. Due to the fact that Solidity truncates integers
+when dividing, the result of price / 100 will be 0 if the price is less than 100. 
+This causes the result of the multiplication to be 0 as well.
+
+On the other hand, in the Calculation contract, the function price performs the multiplication
+before the division (price * discount) / 100. This way, the result will be correct as the multiplication
+doesn't get truncated, only the final result does.
+
+Mitigation:Always perform multiplication before division to avoid losing precision.
+
+REF:
+https://twitter.com/1nf0s3cpt/status/1599774264437395461
+https://blog.solidityscan.com/precision-loss-in-arithmetic-operations-8729aea20be9
+
+*/
 contract ContractTest is Test {
     Miscalculation MiscalculationContract;
     Calculation CalculationContract;
