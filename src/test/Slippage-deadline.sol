c6decac2a08f7219debcb31ee3ed832ab6ecdca5
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-07-22 21:55:03 +0800

    Update Slippage-deadline.sol

--- src/test/Slippage-deadline.sol
+++ src/test/Slippage-deadline.sol
@@ -5,7 +5,9 @@ import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /*
-Demo: Slippage - Incorrect deadline & slippage amount
+Name: Slippage - Incorrect deadline & slippage amount
+
+Description:
 Slippage: Slippage is the difference between the expected price of a trade 
 and the price at which the trade is executed. 
 If hardcoded to 0, user will accept a minimum amount of 0 output tokens from the swap.
@@ -16,11 +18,11 @@ which means the transaction can be executed at any time.
 If slippage is set to 0 and there is no deadline, 
 users might potentially lose all their tokens.
 
-Mitigation
+Mitigation:
 Allow the user to specify the slippage & deadline value themselves.
 
-REF
- 
+REF:
+https://twitter.com/1nf0s3cpt/status/1676118132992405505
 */
 
 interface IUniswapV2Router02 {
